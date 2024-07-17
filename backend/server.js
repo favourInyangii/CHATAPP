8334eba077ea9d34ca643ffcc300aadaf2c7576b
favour ololade <ololadefavourajayi@gmail.com>
2024-07-17 16:00:07 +0100

    Authentication successfull

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,19 @@ import connectToMongoDB from "./Db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
-
-dotenv.config();    
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) => {
-    res.send("last one i will be doing ");
-})
+dotenv.config(); 
 
+app.use (express.json());//to parse the incoming requests with JSON payloads(from req.body)
 
 app.use("/api/auth", authRoutes)
+
+//app.get("/", (req,res) => {
+   // res.send("last one i will be doing ");
+//})
+
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
