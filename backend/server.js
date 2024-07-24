d4e3a197fcb99b6a418ad4ec28a990b37846f9df
favour ololade <ololadefavourajayi@gmail.com>
2024-07-24 21:16:23 +0100

    testing the message controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ dotenv.config();
 app.use (express.json());//to parse the incoming requests with JSON payloads(from req.body)
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 //app.get("/", (req,res) => {
    // res.send("last one i will be doing ");
